ce7b7881a1015d4e0f60a145a8ba501de249ac94
mirzakomic <komic.mirza@gmail.com>
2024-06-14 13:23:38 +0200

    added modal to delete entry

--- frontend/src/components/Entries.jsx
+++ frontend/src/components/Entries.jsx
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Button from './Button.jsx';
+import 'react-responsive-modal/styles.css';
+import { Modal } from 'react-responsive-modal';
 
 const DiaryEntries = () => {
   const [entries, setEntries] = useState([]);
+  const [open, setOpen] = useState(false);
+
+  const onOpenModal = () => setOpen(true);
+  const onCloseModal = () => setOpen(false);
 
   useEffect(() => {
     const fetchEntries = async () => {
@@ -24,6 +30,7 @@ const DiaryEntries = () => {
       console.log("clicked delete");
       setEntries(entries.filter(entry => entry._id !== id));
       console.log("deleted?");
+      setOpen(false);
     } catch (error) {
       console.error('Failed to delete diary entry', error);
     }
@@ -39,9 +46,13 @@ const DiaryEntries = () => {
           <p>{entry.content}</p>
           <p className='bg-darkBabyBlue text-secondary py-2'>Mood: {['😞', '😕', '😐', '😊', '😁'][entry.mood - 1]}</p>
           <p>{new Date(entry.date).toLocaleString()}</p>
-              <Button variant="primary" size="small" shape="round" onClick={() => deleteEntry(entry._id)}>
+              <Button variant="primary" size="small" shape="round" onClick={onOpenModal}>
               🗑️
             </Button>
+      <Modal open={open} onClose={onCloseModal} center>
+        <h2 className='text-primary mt-10 mb-5'>Are you sure that you want to delete this entry?</h2>
+        <Button variant="secondary" size="big" shape="round" onClick={() => deleteEntry(entry._id)}>Yes</Button>
+      </Modal>
         </div>
       ))}
     </div>
